4442a4277b25d44d388acb86d0f36bdfe4d24f53
gurunhuel <erwanleguillou@gmail.com>
2016-07-22 09:48:07 +0200

    Created drbLib.h

---
+++ drbfs/drbLib.h
@@ -0,0 +1,13 @@
+/*
+ * drbLib.h
+ *
+ *  Created on: Apr 18, 2016
+ *      Author: gurunhuel
+ */
+
+#ifndef DRBLIB_H_
+#define DRBLIB_H_
+
+extern STATUS drbCreate (char *, char *, char *, char *);
+
+#endif /* DRBLIB_H_ */
